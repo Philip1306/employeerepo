d92dedf45388e809bdaa085ebcc84281a488a65f
Philip <philip@1306.com>
2022-05-23 14:55:50 +0530

    first commit

---
+++ dict_demo.py
@@ -0,0 +1,5 @@
+Empty_dic={}
+print(len(Empty_dic))
+crew_details={"Pilot":"Kumar","Co-Pilot":"Raghav","Head-Stewardess":"Malini","Stewardess":"Mala"}
+print(crew_details)
+print(crew_details["Pilot"])
